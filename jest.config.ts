e0eee0d52f04205caa1b84a2639b6fed0ddea047
Sarah Souza <sarahsouza.aluna@gmail.com>
2024-11-06 22:40:23 -0300

    feat: add git hooks setup and font

---
+++ jest.config.ts
@@ -0,0 +1,4 @@
+module.exports = {
+    testMatch: ["**/tests/**/*.[jt]s?(x)"], // Altere "tests" para o nome da sua pasta de testes, se necessário
+    // Aqui você pode adicionar outras configurações personalizadas
+  };
