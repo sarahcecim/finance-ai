3710ca700dc4b5fbb5f558f0854f0c3a2094adbf
Sarah Souza <sarahsouza.aluna@gmail.com>
2024-11-13 20:43:56 -0300

    fix: upsert transaction function

--- app/_actions/add-transaction/index.ts
+++ app/_actions/add-transaction/index.ts
@@ -1,12 +1,12 @@
 "use server";
 
+import { db } from "@/app/_lib/prisma";
+import { auth } from "@clerk/nextjs/server";
 import {
   TransactionCategory,
   TransactionPaymentMethod,
   TransactionType,
 } from "@prisma/client";
-import { db } from "../../_lib/prisma";
-import { auth } from "@clerk/nextjs/server";
 import { upsertTransactionSchema } from "./schema";
 import { revalidatePath } from "next/cache";
 
@@ -21,31 +21,17 @@ interface UpsertTransactionParams {
 }
 
 export const upsertTransaction = async (params: UpsertTransactionParams) => {
-  upsertTransactionSchema.parse(params);  // Validando os dados
-
+  upsertTransactionSchema.parse(params);
   const { userId } = await auth();
   if (!userId) {
     throw new Error("Unauthorized");
   }
-
-  const transactionData = { ...params, userId };
-
-  if (params.id) {
-    // Se o ID estiver presente, faz o upsert (atualiza ou cria)
-    await db.transaction.upsert({
-      where: {
-        id: params.id,  // Verifique se o ID está presente antes de usar no where
-      },
-      update: transactionData,  // Atualiza a transação existente
-      create: transactionData,  // Cria uma nova transação se não existir
-    });
-  } else {
-    // Se o ID não estiver presente, cria uma nova transação
-    await db.transaction.create({
-      data: transactionData,  // Cria uma nova transação
-    });
-  }
-
-  // Revalida a rota para refletir as mudanças
+  await db.transaction.upsert({
+    update: { ...params, userId },
+    create: { ...params, userId },
+    where: {
+      id: params?.id ?? "",
+    },
+  });
   revalidatePath("/transactions");
 };
